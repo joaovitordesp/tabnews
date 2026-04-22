0d5bf98e8078c88c1ee139a9665edad7916039a8
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2026-04-22 21:36:46 +0000

    add message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-   return <h1>Se você olhou de novo esse link, dá um sorrisinho dá?</h1>
+   return <h1>Se você olhou de novo esse link, dá um sorrisinho dá? <h2>Deu?</h2> </h1> 
+
 }
 
 
