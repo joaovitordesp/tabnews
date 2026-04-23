2cff1244bb6cce867895e53b798eca4758924577
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2026-04-23 13:53:20 +0000

    lint: lint no code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-function Home(){
-   return <h1>Se você olhou de novo esse link, dá um sorrisinho dá? <h2>Deu?</h2> </h1> 
-
+function Home() {
+  return (
+    <h1>
+      Se você olhou de novo esse link, dá um sorrisinho dá? <h2>Deu?</h2>{" "}
+    </h1>
+  );
 }
-  
 
-export default Home; 
+export default Home;
