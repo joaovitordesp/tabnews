903eae58bfb8f1c486f84d96a77ab828f9ec7d5c
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2026-04-30 18:35:29 +0000

    fix: fix status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,37 +1,40 @@
 import database from "../../../../infra/database";
 
 async function status(req, res) {
-  const updateAt = new Date().toISOString();
+  try {
+    const updateAt = new Date().toISOString();
 
-  const databaseVersion = await database.query("SHOW server_version();");
-  const databaseVersionValue = databaseVersion.rows[0].server_version;
+    const databaseVersion = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersion[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-  console.log(databaseMaxConnectionsValue);
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenResult = await database.query({
-    text: "SELECT count(*)::int from pg_stat_activity where datname= $1;",
-    values: [databaseName],
-  });
-  // "SELECT count(*)::int from pg_stat_activity where datname='local_db';",
-  const databaseOpenConnectionsValue = databaseOpenResult.rows[0].count;
+    const databaseMaxConnectionsResult = await database.query(
+      "SHOW max_connections",
+    );
+    const databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult[0].max_connections;
+    // console.log(databaseMaxConnectionsValue);
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenResult = await database.query({
+      text: "SELECT count(*)::int from pg_stat_activity where datname = $1;",
+      values: [databaseName],
+    });
+    // "SELECT count(*)::int from pg_stat_activity where datname='local_db';",
+    const databaseOpenConnectionsValue = databaseOpenResult[0].count;
 
-  res.status(200).json({
-    updated_at: updateAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenConnectionsValue,
+    res.status(200).json({
+      updated_at: updateAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionsValue),
+          opened_connections: databaseOpenConnectionsValue,
+        },
       },
-    },
-  });
-
-  return;
+    });
+  } catch (error) {
+    console.error("Error fetching status:", error);
+    res.status(500).json({ error: "Failed to fetch status" });
+  }
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependecies.database).toEqual("16.0");
-  expect(responseBody.dependecies.max_connections).toEqual(100);
-  expect(responseBody.dependecies.opened_connections).toEqual(1);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
